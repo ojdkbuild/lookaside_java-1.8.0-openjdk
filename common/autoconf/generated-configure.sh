e91b181d4ad771df5285710ee0651498f94d21e7
akashche <akashche@redhat.com>
2019-01-17 06:40:39 -0800

    toolchain detection fix rework

--- common/autoconf/generated-configure.sh
+++ common/autoconf/generated-configure.sh
@@ -25602,7 +25602,7 @@ $as_echo "$as_me: directory within the Visual Studio installation" >&6;}
     fi
   fi
 
-  VS_ENV_CMD=""
+  VS_ENV_CMD=OJDKBUILD
 
   if test "x$VS_COMNTOOLS" != x; then
 
@@ -26153,7 +26153,7 @@ $as_echo "$as_me: Found $VS_DESCRIPTION" >&6;}
 
   # If we have a devkit, skip all of the below.
   if test "x$DEVKIT_VS_VERSION" = x; then
-    if test "x$VS_ENV_CMD" != x; then
+    if test "x$VS_ENV_CMD" != xOJDKBUILD; then
       # We have found a Visual Studio environment on disk, let's extract variables from the vsvars bat file.
 
   if test "x$OPENJDK_BUILD_OS_ENV" = "xwindows.cygwin"; then
@@ -26578,10 +26578,10 @@ $as_echo "$as_me: or run \"bash.exe -l\" from a VS command prompt and then run c
 
     # Reset path to VS_PATH. It will include everything that was on PATH at the time we
     # ran TOOLCHAIN_SETUP_VISUAL_STUDIO_ENV.
-    PATH="$VS_PATH"
+    # PATH="$VS_PATH"
     # The microsoft toolchain also requires INCLUDE and LIB to be set.
-    export INCLUDE="$VS_INCLUDE"
-    export LIB="$VS_LIB"
+    # export INCLUDE="$VS_INCLUDE"
+    # export LIB="$VS_LIB"
   fi
 
   # Before we locate the compilers, we need to sanitize the Xcode build environment
