5974ce1e8a661492a6f8c1068e49c9a930711851
akashche <akashche@redhat.com>
2019-02-21 22:59:30 +0000

    apply pr1834-rh1022017-reduce_ellipticcurvesextension_to_provide_only_three_nss_supported_nist_curves_23_24_25

--- jdk/src/share/classes/sun/security/ssl/EllipticCurvesExtension.java
+++ jdk/src/share/classes/sun/security/ssl/EllipticCurvesExtension.java
@@ -145,20 +145,10 @@ final class EllipticCurvesExtension extends HelloExtension {
                     "contains no supported elliptic curves");
             }
         } else {        // default curves
-            int[] ids;
-            if (requireFips) {
-                ids = new int[] {
-                    // only NIST curves in FIPS mode
-                    23, 24, 25, 9, 10, 11, 12, 13, 14,
-                };
-            } else {
-                ids = new int[] {
-                    // NIST curves first
-                    23, 24, 25, 9, 10, 11, 12, 13, 14,
-                    // non-NIST curves
-                    22,
-                };
-            }
+            int[] ids = new int[] { 
+		// NSS currently only supports these three NIST curves
+		23, 24, 25
+	    };
 
             idList = new ArrayList<>(ids.length);
             for (int curveId : ids) {
