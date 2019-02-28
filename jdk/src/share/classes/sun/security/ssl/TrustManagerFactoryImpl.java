b9af3e11305fac6346f57a2eff835b76f41461f3
akashche <akashche@redhat.com>
2019-02-28 09:04:08 -0800

    revert pr2888-openjdk_should_check_for_system_cacerts_database_eg_etc_pki_java_cacerts
    This reverts commit 18154877dbf3c02fd9a3cb0185960c5e104f43cf.

--- jdk/src/share/classes/sun/security/ssl/TrustManagerFactoryImpl.java
+++ jdk/src/share/classes/sun/security/ssl/TrustManagerFactoryImpl.java
@@ -174,20 +174,15 @@ abstract class TrustManagerFactoryImpl extends TrustManagerFactorySpi {
                     storeFile = new File(storeFileName);
                     fis = getFileInputStream(storeFile);
                 } else {
-                    /* Check system cacerts DB first; /etc/pki/java/cacerts */
-                    storeFile = new File(sep + "etc" + sep + "pki" + sep
-                                         + "java" + sep + "cacerts");
+                    String javaHome = props.get("javaHome");
+                    storeFile = new File(javaHome + sep + "lib" + sep
+                                                    + "security" + sep +
+                                                    "jssecacerts");
                     if ((fis = getFileInputStream(storeFile)) == null) {
-                        String javaHome = props.get("javaHome");
                         storeFile = new File(javaHome + sep + "lib" + sep
-                                             + "security" + sep +
-                                             "jssecacerts");
-                        if ((fis = getFileInputStream(storeFile)) == null) {
-                            storeFile = new File(javaHome + sep + "lib" + sep
-                                                 + "security" + sep +
-                                                 "cacerts");
-                            fis = getFileInputStream(storeFile);
-                        }
+                                                    + "security" + sep +
+                                                    "cacerts");
+                        fis = getFileInputStream(storeFile);
                     }
                 }
 
