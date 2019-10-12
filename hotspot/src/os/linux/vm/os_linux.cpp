6f40fac7e184ff47f1ea273d8de01cbfd05d5289
akashche <akashche@redhat.com>
2019-10-12 12:25:05 +0100

    apply rh1566890-CVE_2018_3639-speculative_store_bypass_toggle

--- hotspot/src/os/linux/vm/os_linux.cpp
+++ hotspot/src/os/linux/vm/os_linux.cpp
@@ -5002,26 +5002,43 @@ const char* os::exception_name(int exception_code, char* buf, size_t size) {
 
 /* Per task speculation control */
 #ifndef PR_GET_SPECULATION_CTRL
-#define PR_GET_SPECULATION_CTRL    52
+# define PR_GET_SPECULATION_CTRL    52
 #endif
 #ifndef PR_SET_SPECULATION_CTRL
-#define PR_SET_SPECULATION_CTRL    53
+# define PR_SET_SPECULATION_CTRL    53
 #endif
 /* Speculation control variants */
-# undef PR_SPEC_STORE_BYPASS
+#ifndef PR_SPEC_STORE_BYPASS
 # define PR_SPEC_STORE_BYPASS          0
+#endif
 /* Return and control values for PR_SET/GET_SPECULATION_CTRL */
-# undef PR_SPEC_NOT_AFFECTED
-# undef PR_SPEC_PRCTL
-# undef PR_SPEC_ENABLE
-# undef PR_SPEC_DISABLE
+
+#ifndef PR_SPEC_NOT_AFFECTED
 # define PR_SPEC_NOT_AFFECTED          0
+#endif
+#ifndef PR_SPEC_PRCTL
 # define PR_SPEC_PRCTL                 (1UL << 0)
+#endif
+#ifndef PR_SPEC_ENABLE
 # define PR_SPEC_ENABLE                (1UL << 1)
+#endif
+#ifndef PR_SPEC_DISABLE
 # define PR_SPEC_DISABLE               (1UL << 2)
+#endif
+#ifndef PR_SPEC_FORCE_DISABLE
+# define PR_SPEC_FORCE_DISABLE         (1UL << 3)
+#endif
+#ifndef PR_SPEC_DISABLE_NOEXEC
+# define PR_SPEC_DISABLE_NOEXEC        (1UL << 4)
+#endif
 
 static void set_speculation() __attribute__((constructor));
 static void set_speculation() {
+  if ( prctl(PR_SET_SPECULATION_CTRL,
+             PR_SPEC_STORE_BYPASS,
+             PR_SPEC_DISABLE_NOEXEC, 0, 0) == 0 ) {
+    return;
+  }
   prctl(PR_SET_SPECULATION_CTRL, PR_SPEC_STORE_BYPASS, PR_SPEC_DISABLE, 0, 0);
 }
 
