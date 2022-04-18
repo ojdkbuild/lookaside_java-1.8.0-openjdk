f9d5a266d41f8c9c1efde1d8b48fb538483b9487
akashche <akashche@redhat.com>
2022-04-18 12:14:48 -0700

    revert pr2888-openjdk_should_check_for_system_cacerts_database_eg_etc_pki_java_cacerts
    This reverts commit 49a7c0c5f317e1957cf6acfbd553f3972ac2100d.

--- jdk/src/share/classes/sun/security/ssl/TrustStoreManager.java
+++ jdk/src/share/classes/sun/security/ssl/TrustStoreManager.java
@@ -68,7 +68,7 @@ final class TrustStoreManager {
      * The preference of the default trusted KeyStore is:
      *    javax.net.ssl.trustStore
      *    jssecacerts
-     *    cacerts (system and local)
+     *    cacerts
      */
     private static final class TrustStoreDescriptor {
         private static final String fileSep = File.separator;
@@ -79,10 +79,6 @@ final class TrustStoreManager {
                 defaultStorePath + fileSep + "cacerts";
         private static final String jsseDefaultStore =
                 defaultStorePath + fileSep + "jssecacerts";
-        /* Check system cacerts DB: /etc/pki/java/cacerts */
-        private static final String systemStore =
-                fileSep + "etc" + fileSep + "pki" +
-                fileSep + "java" + fileSep + "cacerts";
 
         // the trust store name
         private final String storeName;
@@ -148,8 +144,7 @@ final class TrustStoreManager {
                     long temporaryTime = 0L;
                     if (!"NONE".equals(storePropName)) {
                         String[] fileNames =
-                                new String[] {storePropName,
-                                              systemStore, defaultStore};
+                                new String[] {storePropName, defaultStore};
                         for (String fileName : fileNames) {
                             File f = new File(fileName);
                             if (f.isFile() && f.canRead()) {
