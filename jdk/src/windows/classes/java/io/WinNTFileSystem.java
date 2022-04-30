0ff6e3dca05cf0464d6a61422a016bca3e33c906
akashche <akashche@redhat.com>
2022-04-30 14:13:34 -0700

    revert JDK-8285445
    This reverts commit 457d089aa690d9c60fa664f55d75623141c0a05b.

--- jdk/src/windows/classes/java/io/WinNTFileSystem.java
+++ jdk/src/windows/classes/java/io/WinNTFileSystem.java
@@ -56,15 +56,16 @@ class WinNTFileSystem extends FileSystem {
 
     // Whether to enable alternative data streams (ADS) by suppressing
     // checking the path for invalid characters, in particular ":".
-    // By default, ADS support is enabled and will be disabled if and
-    // only if the property is set, ignoring case, to the string "false".
+    // ADS support will be enabled if and only if the property is set and
+    // is the empty string or is equal, ignoring case, to the string "true".
+    // By default ADS support is disabled.
     private static final boolean ENABLE_ADS;
     static {
         String enableADS = GetPropertyAction.privilegedGetProperty("jdk.io.File.enableADS");
         if (enableADS != null) {
-            ENABLE_ADS = !enableADS.equalsIgnoreCase(Boolean.FALSE.toString());
+            ENABLE_ADS = "".equals(enableADS) || Boolean.parseBoolean(enableADS);
         } else {
-            ENABLE_ADS = true;
+            ENABLE_ADS = false;
         }
     }
 
